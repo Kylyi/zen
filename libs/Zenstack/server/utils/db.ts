da3c0b8a807d9955fe881720c0e8c0f3a92ba39e
Kylian, Jakub <jacob.kylian@gmail.com>
2025-12-16 04:36:48 +0100

    removed unnecessary stuff

--- libs/Zenstack/server/utils/db.ts
+++
@@ -1,9 +0,0 @@
-import { ZenStackClient } from '@zenstackhq/orm'
-import { PostgresDialect } from 'kysely'
-import { Pool } from 'pg'
-
-const connectionString = import.meta.env.DB_URL
-
-export const db = new ZenStackClient(schema, {
-  dialect: new PostgresDialect({ pool: new Pool({ connectionString }) }),
-})
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   extends: [
-    './libs/User',
-    './libs/Core',
-    './libs/Zenstack',
     './libs/Utilities',
   ],
 
